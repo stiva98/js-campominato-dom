b6b044b09c9ff71201482d7ec6aa09c073760218
stiva98 <samustiva9810@gmail.com>
2023-06-22 16:01:28 +0200

    ADD generazione di 16 numeri casuali da 1 a 100

--- js/scripts.js
+++ js/scripts.js
@@ -41,3 +41,25 @@ reset.addEventListener('click', function (){
     gridContainer.innerHTML = '';
 
 })
+
+//Generazione di 16 numeri casuali e non ripetuti nella stessa cella
+
+const bomb = 16;
+
+const arrBombRandom = [];
+while (arrBombRandom.length < bomb){
+
+    const newBomb = randomNumber(1,100);
+    if (arrBombRandom.includes(newBomb) == false) {
+        arrBombRandom.push(newBomb);
+        console.log(newBomb)
+    }
+}
+
+
+16
+
+
+function randomNumber(min,max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
